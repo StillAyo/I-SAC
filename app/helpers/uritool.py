4aff086a60e2e51edcca5ed551947191f7eaa0d1
ld558 <45179374+ld558@users.noreply.github.com>
2019-08-20 14:54:16 +0100

    Update uritool.py

--- app/helpers/uritool.py
+++ app/helpers/uritool.py
@@ -26,9 +26,9 @@ class URItool:
                 events["data"].append(event_json[i]['_source']['data'])
             except:
                 del event_json[i]
-        filepath = os.path.join('/tisac', 'searchresults')
-        if not os.path.exists('/tisac'):
-            os.makedirs('/tisac')
+        filepath = os.path.join('/home/vagrant/I-SAC', 'results.json')
+        if not os.path.exists('/home/vagrant/I-SAC'):
+            os.makedirs('/home/vagrant/I-SAC')
 
         with open(filepath, 'a')as f:
             f.truncate(0)
