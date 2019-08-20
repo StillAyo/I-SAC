c1ea6a3c5292f7f13ef006fb43996d6c562ecb5f
StillAyo <52698719+StillAyo@users.noreply.github.com>
2019-08-20 14:53:18 +0100

    new new

--- app/routes.py
+++ app/routes.py
@@ -231,6 +231,7 @@ def explore():
             if request.form.get(x) is not None:
                 print(request.form.get(x))
                 temp2.append(x)
+                x=3
         filter['orgName'] = temp2
 
         for x in tlp_choices:
