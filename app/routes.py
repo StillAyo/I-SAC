444d53b4dc2b90be7f5896150897aeaedce73065
StillAyo <52698719+StillAyo@users.noreply.github.com>
2019-08-20 14:06:11 +0100

    new new

--- app/routes.py
+++ app/routes.py
@@ -214,7 +214,7 @@ def explore():
             category_choices = ["malware-check","dos-check","vulnerability-check","infoleak-check"]
             temp=[]
             for x in category_choices:
-                if request.form.get(x) is not None:
+                if request.form.get(x) != "None":
                     print(request.form.get(x))
                     #temp.append(request.form.get(x))
             # filter['Organisation'] = temp
