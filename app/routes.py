b569f08f1bf750174bdecf330a3f759610c90926
StillAyo <52698719+StillAyo@users.noreply.github.com>
2019-08-20 13:51:34 +0100

    new new

--- app/routes.py
+++ app/routes.py
@@ -209,9 +209,12 @@ def home():
 @app.route('/explore', methods=["POST", "GET"])
 def explore():
     if request.method == "POST":
-        testing = request.form.get("malware-check")
-        print(testing)
+        if len(request.form["searchInput"]) == 0:
+            print("nothing searched")
+        # testing = request.form.get["malware-check"]
+        # print(testing)
         # print(request.form["malware-check"])
+        #testing = request.form["searchInput"]
         return redirect("/explore", code=302)
     else:
         ## misp feed, with data saved in json file
