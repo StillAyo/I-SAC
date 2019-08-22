500071cf44c8e970a50ddd63d9fad3d8091ec81a
StillAyo <52698719+StillAyo@users.noreply.github.com>
2019-08-22 09:15:17 +0100

    new new

--- app/static/styles/toolkit.js
+++ app/static/styles/toolkit.js
@@ -3,11 +3,11 @@ function format ( d ) {
     // `d` is the original data object for the row
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
-            '<td>Full description:</td>'+
+            '<td>Mitre-Attack Name:</td>'+
             '<td>Test</td>'+
         '</tr>'+
         '<tr>'+
-            '<td>Tags:</td>'+
+            '<td>Description:</td>'+
             '<td>Test</td>'+
         '</tr>'
 
